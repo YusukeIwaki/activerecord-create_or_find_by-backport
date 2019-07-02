0035660c18506299050b746459b15b08b3b8de6e
YusukeIwaki <yusuke-iwaki@example.com>
2019-07-02 17:15:34 +0000

    bundle gem

---
+++ lib/activerecord/create_or_find_by/backport.rb
@@ -0,0 +1,10 @@
+require "activerecord/create_or_find_by/backport/version"
+
+module Activerecord
+  module CreateOrFindBy
+    module Backport
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/activerecord/create_or_find_by/backport/version.rb
@@ -0,0 +1,7 @@
+module Activerecord
+  module CreateOrFindBy
+    module Backport
+      VERSION = "0.1.0"
+    end
+  end
+end
